389171dd25ce1e0e79c7a20ba09192369b54c134
KevinBarreda <kevin.barreda@tecsup.edu.pe>
2021-12-02 20:17:16 -0500

    lab2

---
+++ Lab02-01/frmLogin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_01
+{
+    public partial class frmLogin : Form
+    {
+        private string email = "wilson.turpo@tecsup.edu.pe";
+        private string password = "Tecsup100";
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            string lblemail = txtUsuario.Text;
+            string lblpassword = txtPassword.Text;
+            if (lblemail.Equals(email) && lblpassword.Equals(password))
+            {
+                PrincipalMDI principal = new PrincipalMDI();
+                principal.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Las credenciales no son correctas");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
